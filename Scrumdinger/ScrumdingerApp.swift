9dcb7ef568a795d5bc7e12b7cf5ee469903a2b5c
akula <akulinind@gmail.com>
2022-07-25 12:18:27 +0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Dmytro Akulinin on 25.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
