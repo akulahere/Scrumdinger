e029905917ad0d54118b7ab399b17eccdaaabc4f
akula <akulinind@gmail.com>
2022-08-02 10:54:10 +0300

    feat: add data persistence

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -7,12 +7,13 @@
 
 import SwiftUI
 
-struct DailyScrum: Identifiable {
+struct DailyScrum: Identifiable, Codable {
   let id: UUID
   var title: String
   var attendees: [Attendee]
   var lengthInMinutes: Int
   var theme: Theme
+  var history: [History] = []
   
   init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
     self.id = id
@@ -24,7 +25,7 @@ struct DailyScrum: Identifiable {
 }
 
 extension DailyScrum {
-  struct Attendee: Identifiable {
+  struct Attendee: Identifiable, Codable {
     let id: UUID
     var name: String
     
@@ -44,14 +45,28 @@ extension DailyScrum {
   var data: Data {
     Data(title: title, attendees: attendees, lengthInMinutes: Double(lengthInMinutes), theme: theme)
   }
+  
+  mutating func update(from data: Data) {
+    title = data.title
+    attendees = data.attendees
+    lengthInMinutes = Int(data.lengthInMinutes)
+    theme = data.theme
+  }
+  
+  init(data: Data) {
+    id = UUID()
+    title = data.title
+    attendees = data.attendees
+    lengthInMinutes = Int(data.lengthInMinutes)
+    theme = data.theme
+  }
 }
 
 extension DailyScrum {
-  static var sampleData: [DailyScrum] {
-    [
-      DailyScrum(title: "Design", attendees: ["Cathy", "Daisy", "Simon", "Jonathan"], lengthInMinutes: 10, theme: .yellow),
-      DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"], lengthInMinutes: 5, theme: .orange),
-      DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 5, theme: .poppy)
-    ]
-  }
+  static let sampleData: [DailyScrum] =
+  [
+    DailyScrum(title: "Design", attendees: ["Cathy", "Daisy", "Simon", "Jonathan"], lengthInMinutes: 10, theme: .yellow),
+    DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"], lengthInMinutes: 5, theme: .orange),
+    DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 5, theme: .poppy)
+  ]
 }
---
+++ Scrumdinger/Models/History.swift
@@ -0,0 +1,23 @@
+//
+//  History.swift
+//  Scrumdinger
+//
+//  Created by Dmytro Akulinin on 02.08.2022.
+//
+
+import Foundation
+
+struct History: Identifiable, Codable {
+  let id: UUID
+  let date: Date
+  var attendees: [DailyScrum.Attendee]
+  var lengthInMinutes: Int
+  
+  init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee], lengthInMinutes: Int = 5) {
+    self.id = id
+    self.date = date
+    self.attendees = attendees
+    self.lengthInMinutes = lengthInMinutes
+  }
+}
+
